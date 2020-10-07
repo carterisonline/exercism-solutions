7c6727291d6f3586be50dea9f5930355c22363df
Carter Reeb <carter@localhost.localdomain>
2020-10-07 17:55:39 -0400

    Anagram Version 2

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -1,9 +1,38 @@
 use std::collections::HashSet;
 
-pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&str]) -> HashSet<&'a str> {
-    unimplemented!(
-        "For the '{}' word find anagrams among the following words: {:?}",
-        word,
-        possible_anagrams
-    );
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &'a [&str]) -> HashSet<&'a str> {
+    let v: Vec<bool> = possible_anagrams
+        .into_iter()
+        .map(|s| {
+            &s.len() == &word.len()
+                && &s.to_uppercase() != &word.to_uppercase()
+                && &word
+                    .chars()
+                    .map(|c| {
+                        &s.to_uppercase()
+                            .matches(&c.to_uppercase().to_string())
+                            .collect::<Vec<&str>>()
+                            .len()
+                            == &word
+                                .to_uppercase()
+                                .matches(&c.to_uppercase().to_string())
+                                .collect::<Vec<&str>>()
+                                .len()
+                    })
+                    .collect::<Vec<bool>>()
+                    .contains(&false)
+                    == &false
+        })
+        .collect();
+    let mut out: HashSet<&'a str> = HashSet::new();
+    println!("{:?}", v);
+    possible_anagrams
+        .into_iter()
+        .enumerate()
+        .for_each(|(i, s)| {
+            if &v.get(i).unwrap() == &&true {
+                out.insert(s);
+            }
+        });
+    out
 }
