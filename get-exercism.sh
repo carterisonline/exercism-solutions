e4e987011cc48327e7bed5e4b4841188f113534f
carterisonline <carter.reeb.developer@gmail.com>
2020-10-09 14:43:26 +0000

    Removed shell script with sensitive info.

---
+++ get-exercism.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Run `source get-exercism.sh` in order to update environment variables
+
+eval $(gp env -e)
+
+mkdir temp
+cd temp
+
+wget https://github.com/exercism/cli/releases/download/v3.0.13/exercism-3.0.13-linux-x86_64.tar.gz
+tar -xf exercism-3.0.13-linux-x86_64.tar.gz
+mv exercism ../exercism
+
+cd ..
+rm temp -R
+
+mkdir programs
+mv exercism programs/exercism
+export PATH=$PATH:./programs
+
+cd programs
+chmod +x exercism
+./exercism configure --token=$EXERCISM_KEY
+./exercism configure --workspace=.
+
+cd ..
