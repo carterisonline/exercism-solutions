ae68ec8c0784ed4a4b398a220d31f824d43445d4
carterisonline <carter.reeb.developer@gmail.com>
2020-10-15 15:49:59 +0000

    Sublist - Update 1

--- rust/sublist/src/lib.rs
+++ rust/sublist/src/lib.rs
@@ -6,6 +6,40 @@ pub enum Comparison {
     Unequal,
 }
 
+macro_rules! sub {
+    ($list_1:expr, $list_2:expr, $replacement:expr, $result:expr) => {
+        $list_1
+            .into_iter()
+            .enumerate()
+            .filter(|(i, e)| match &$list_2.get(0) {
+                Some(_) => {
+                    e == &$list_1.get(*i).unwrap() && $list_1.get(*i..(i + $list_2.len())).is_some()
+                }
+                None => false,
+            })
+            .map(|(i, _e)| $list_1.get(i..(i + $list_2.len())).unwrap())
+            .for_each(|x| {
+                if x == $list_2 {
+                    $replacement = $result;
+                }
+            });
+    };
+}
+
 pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Comparison {
-    unimplemented!("Determine if the first list is equal to, sublist of, superlist of or unequal to the second list.");
+    if _first_list == _second_list {
+        return Comparison::Equal;
+    }
+    if _first_list.len() == 0 {
+        return Comparison::Sublist;
+    }
+    if _second_list.len() == 0 {
+        return Comparison::Superlist;
+    }
+
+    let mut result = Comparison::Unequal;
+    sub!(_first_list, _second_list, result, Comparison::Superlist);
+    sub!(_second_list, _first_list, result, Comparison::Sublist);
+
+    result
 }
