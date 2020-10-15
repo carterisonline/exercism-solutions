48313e305ab8f607b63e43c07c72332d1d38c246
carterisonline <carter.reeb.developer@gmail.com>
2020-10-15 15:58:23 +0000

    Better descriptions for the automated setup command

--- get-exercism.sh
+++ get-exercism.sh
@@ -4,22 +4,33 @@
 
 eval $(gp env -e)
 
+# Temporary Directory for downloaded tarballs & programs to reside
 mkdir temp
 cd temp
 
+# Download the Exercism Client
 wget https://github.com/exercism/cli/releases/download/v3.0.13/exercism-3.0.13-linux-x86_64.tar.gz
+
+# Extract tarball and move program out of temp directory
 tar -xf exercism-3.0.13-linux-x86_64.tar.gz
 mv exercism ../exercism
 
 cd ..
-rm temp -R
 
+# Clean it up
+rm temp -R
 mkdir programs
 mv exercism programs/exercism
+
+# Export to the `$PATH` variable: !ONLY WORKS WHEN RUNNING `source exercism.sh`!
 export PATH=$PATH:$GITPOD_REPO_ROOT/programs
 
 cd programs
+
+# Allow Execution of the Exercism CLI
 chmod +x exercism
+
+# Provide Exercism with the token; run `gp env EXERCISM_KEY=(your key here)` to sync with your user profile
 ./exercism configure --token=$EXERCISM_KEY
 ./exercism configure --workspace=..
 
