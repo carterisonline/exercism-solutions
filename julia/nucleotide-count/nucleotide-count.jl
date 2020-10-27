7f106bf094584f0f9499f9deb51ec15a5a11bba3
carterisonline <carter.reeb.developer@gmail.com>
2020-10-27 15:32:28 +0000

    nucleotide-count update 1

--- julia/nucleotide-count/nucleotide-count.jl
+++ julia/nucleotide-count/nucleotide-count.jl
@@ -7,5 +7,14 @@ Invalid strands raise a `DomainError`.
 
 """
 function count_nucleotides(strand)
-
+    strandcount = Dict('A' => 0, 'C' => 0, 'G' => 0, 'T' => 0)
+    for c in strand
+        try
+            strandcount[c] += 1;
+        catch KeyError
+            throw(DomainError("$c is not a nucleotide"))
+        end
+    end
+    println(strandcount)
+    strandcount
 end
