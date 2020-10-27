b12b2629b16b3f7c7ceb4db781ff53d3d31d3a3c
carterisonline <carter.reeb.developer@gmail.com>
2020-10-27 15:57:08 +0000

    hamming update 1

--- julia/hamming/hamming.jl
+++ julia/hamming/hamming.jl
@@ -1,4 +1,14 @@
 "Your optional docstring here"
 function distance(a, b)
+    if length(a) != length(b)
+        throw(ArgumentError("$a and $b are not the same length and cannot be indexed."))
+    end
 
+    s = length(a)
+    for (i, c) in enumerate(a)
+        if a[i] == b[i]
+            s -= 1
+        end
+    end
+    s
 end
