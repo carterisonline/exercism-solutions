cc45ff2ed646dba3691d7eafae70ce9e7fdee423
Carter Reeb <carter@localhost.localdomain>
2020-10-28 17:46:53 -0400

    bob update 1

--- julia/bob/bob.jl
+++ julia/bob/bob.jl
@@ -1,3 +1,18 @@
-function bob(stimulus)
-
+function bob(stimulus::String)
+    z = 0
+    if endswith(rstrip(stimulus), '?')
+        z = 1
+    elseif isempty(filter(c -> !isspace(c), stimulus))
+        return "Fine. Be that way!"
+    end
+    if uppercase(stimulus) == stimulus && !isempty(filter(c -> isletter(c), stimulus))
+        if z == 1
+            return "Calm down, I know what I'm doing!"
+        else
+            return "Whoa, chill out!"
+        end
+    elseif z == 1
+        return "Sure."
+    end
+    "Whatever."
 end
