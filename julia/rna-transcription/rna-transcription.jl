397570e9983c82533ba466b08f98edf906bca945
Carter Reeb <carter@localhost.localdomain>
2020-10-27 20:26:51 -0400

    rna-transcription update 1

--- julia/rna-transcription/rna-transcription.jl
+++ julia/rna-transcription/rna-transcription.jl
@@ -1,4 +1,11 @@
 function to_rna(dna)
-
+    transcription = Dict('C' => 'G', 'G' => 'C', 'T' => 'A', 'A' => 'U')
+    map((c)
+        ->  try
+                transcription[c]
+            catch KeyError
+                throw(ErrorException("$c is not a valid DNA value"))
+            end,
+        dna
+    )
 end
-
