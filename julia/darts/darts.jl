78f7d16695f9dd994599a1bedc9885506816892b
Carter Reeb <carter@localhost.localdomain>
2020-10-27 21:11:14 -0400

    darts update 1

--- julia/darts/darts.jl
+++ julia/darts/darts.jl
@@ -1,3 +1,12 @@
 function score(x, y)
-
+    distance = √(x^2 + y^2)
+    if distance ≤ 1
+        return 10
+    elseif distance ≤ 5
+        return 5
+    elseif distance ≤ 10
+        return 1
+    else
+        return 0
+    end
 end
