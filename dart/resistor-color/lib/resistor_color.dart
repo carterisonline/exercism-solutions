55f823fa9cd10d1a79f407f1d8d8ac7fd9363db5
carterisonline <carter2k4@protonmail.com>
2021-03-16 14:59:55 +0000

    Passed tests for "Resistor Color"

--- dart/resistor-color/lib/resistor_color.dart
+++ dart/resistor-color/lib/resistor_color.dart
@@ -1,3 +1,18 @@
 class ResistorColor {
-  // Put your code here
+  List<String> get colors => [
+        'black',
+        'brown',
+        'red',
+        'orange',
+        'yellow',
+        'green',
+        'blue',
+        'violet',
+        'grey',
+        'white',
+      ];
+
+  int colorCode(String s) {
+    return this.colors.indexOf(s);
+  }
 }
