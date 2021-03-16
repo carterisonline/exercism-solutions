3fbcc79526ccc86aa9f48d62c59d0c4aa24ffe5a
carterisonline <carter2k4@protonmail.com>
2021-03-16 15:05:04 +0000

    Passed tests for "Two Fer"

--- dart/two-fer/lib/two_fer.dart
+++ dart/two-fer/lib/two_fer.dart
@@ -1,3 +1,7 @@
-String twoFer() {
-  // Put your code here
+String twoFer([String s]) {
+  if (s == null) {
+    return "One for you, one for me.";
+  } else {
+    return "One for $s, one for me.";
+  }
 }
