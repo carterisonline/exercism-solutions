3c713657ad8f42bd21894dfc8f0124a7f9e04640
carterisonline <carter2k4@protonmail.com>
2021-03-16 15:14:28 +0000

    Passed tests for "Resistor Color Duo"

--- dart/resistor-color-duo/lib/resistor_color_duo.dart
+++ dart/resistor-color-duo/lib/resistor_color_duo.dart
@@ -1,3 +1,22 @@
 class ResistorColorDuo {
-  // Put your code here
+  List<String> get resistors => [
+        "black",
+        "brown",
+        "red",
+        "orange",
+        "yellow",
+        "green",
+        "blue",
+        "violet",
+        "grey",
+        "white"
+      ];
+  int value(List<String> list) {
+    int sum = 0;
+
+    sum += this.resistors.indexOf(list[0]) * 10;
+    sum += this.resistors.indexOf(list[1]);
+
+    return sum;
+  }
 }
