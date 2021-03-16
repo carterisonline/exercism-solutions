7bf45e402cafcb3916a79e890b35cdb606efa610
carterisonline <carter2k4@protonmail.com>
2021-03-16 15:33:56 +0000

    Passed tests for "Scrabble Score"

--- dart/scrabble-score/lib/scrabble_score.dart
+++ dart/scrabble-score/lib/scrabble_score.dart
@@ -1,1 +1,25 @@
-// Put your code here
+List<String> wordList = [
+  'aeioulnrst',
+  'dg',
+  'bcmp',
+  'fhvwy',
+  'k',
+  '',
+  '',
+  'jx',
+  '',
+  'qz',
+];
+
+int score(String s) {
+  int out = 0;
+  for(String c in s.split('')) {
+    for(int i = 0; i < wordList.length; i++) {
+      if(wordList[i].contains(c.toLowerCase())) {
+        out += i + 1;
+        break;
+      }
+    }
+  }
+  return out;
+}
